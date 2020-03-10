6a19f781781d6f3a29566273e4e26496b70867d5
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-03-10 18:22:53 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+require 'pry'
+class Backer
+attr_accessor :backed_projects
+attr_reader :name
+def initialize(name)
+  @name = name
+  @backed_projects = []
+end
+
+def back_project(name)
+@backed_projects << name
+name.backers << self
+end
+
+end
